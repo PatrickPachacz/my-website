75c2c78cf844175e47d3973c241d159f8a48a02b
Patrick <patrickpachacz0@gmail.com>
2025-02-21 13:54:08 -0500

    Created JE comp and imported details from there. Cleaning up index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,74 +2,13 @@ import React, { useState } from "react";
 import Loader from "../components/loader"
 import Nav from "../components/nav"
 import "../components/styles.css"; 
+import JobExperience from "../components/jobexperience.js";
+
 
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
-  const [selectedEmployer, setSelectedEmployer] = useState("Angiemellado.art");
-  const [isFading, setIsFading] = useState(false);
-  
-    const jobDescriptions = {
-      "Angiemellado.art": { 
-        title: "Web Developer @ Angiemellado.art", 
-        date: "June 2024 - January 2025", 
-        details: [
-          "Participated in the launching of a responsive, user-centric webpage utilizing WordPress with customized themes, resulting in an increase of client engagement time within the first month of deployment.",
-          "Managed and optimized core web services, leading to a reduction of site downtime.",
-          "Improved major front-end features of Angiemellado.art website.",
-          "Reviewed modifications to ensure thoughtful and consistent user experiences.",
-        ]
-      },
-      "Alan Solutions": { 
-        title: "Frontend Developer @ Alan Solutions", 
-        date: "December 2023 - February 2024", 
-        details: [
-          "Collaborated with designers, project managers, and other engineers to transform creative concepts into production realities for clients.", 
-          "Conducted debugging and testing to ensure data accuracy and functionality.",
-          "Led initiatives to integrate external datasets into the platform, improving user experience for constant flow of traffic.",
-          "Contributed to the creation of web applications utilizing technologies such as Astro, JavaScript, and React."
-        ]
-      },
-      "One Sotheby's": { 
-        title: "Transaction Coordinator @ One Sotheby's International Realty", 
-        date: "May 2023 - May 2024", 
-        details: [
-          "Collaborated with title companies, escrow officers, and other stakeholders to expedite residential closing process.",
-          "Maintained detailed records of transactions and updated databases regularly.",
-          "Communicated effectively with clients, real estate agents, lenders, and other third parties involved in closing transactions.", 
-        ]
-      },
-      "Mortgage Connect": { 
-        title: "Team Lead @ Mortgage Connect", 
-        date: "March 2020 - October 2022", 
-        details: [
-          "Responsible for the coordination of all aspects involving the finalization of the loan closing procoess.",
-          "Managed and supervised a crew of up to 10 employees in the closing department, overseeing daily tasks, workflow efficiency, and staff performance.",
-          "Calculated and applied appropriate fees regarding home refinancing, investments, and mortgage consolidation.",
-          "Prepared Title Bills and Invoicing after reviewing property taxes, judgements, assignments, and other critical documents applicable to final closing." 
-        ]
-        }
-    };
-    
-    const handleEmployerClick = (employer) => {
-      if (employer !== selectedEmployer) {
-        setIsFading(true); 
-        setTimeout(() => {
-          setSelectedEmployer(employer); 
-          setIsFading(false); 
-        }, 300); 
-      }
-    };
 
-    const highlightCompanyName = (title) => {
-    const [jobTitle, companyName] = title.split(" @ ");
-      return (
-      <>
-        {jobTitle} <span style={{ color: 'yellow' }}>{" @ "}{companyName}</span>
-      </>
-    );
-    };
-  
   const styles = {
     page: {
       background: "black",
@@ -122,17 +61,6 @@ export default function Home() {
         padding: "50px",
       },
       
-      sectionHeading: {
-        fontSize: "32px",
-        fontWeight: "bold",
-        textAlign: "center",
-        position: "relative",
-        paddingTop: "50px",
-        width: "100%",
-        maxWidth: "400px",
-        color: "white",
-        borderBottom: "2px solid white", 
-      },
       sectionLine: {
         position: "absolute",
         bottom: "0",
@@ -196,7 +124,7 @@ export default function Home() {
           </div>
 
           <section id="profile" style={styles.section}>
-            <h2 style={styles.sectionHeading}>Profile</h2>
+            <h2 className="sectionHeading">Profile</h2>
             <div class="profileContent">
             <div style={styles.textSection}>
               <p style={styles.profileDescription}>Greetings! My name is Patrick and I enjoy creating web applications. 
@@ -277,43 +205,13 @@ export default function Home() {
             </div>
           </section>
 
-          <div id="experience" className="expContainer">
-          <section class="experience-section">
-            <h2 style={styles.sectionHeading}>Experience</h2>
-            
-          <div class="experience-container">
-            <div class="employer-list">
-            {Object.keys(jobDescriptions).map((employer) => (
-              <button
-                key={employer}
-                className={`employer-item ${selectedEmployer === employer ? "selected" : ""}`}
-                onClick={() => handleEmployerClick(employer)}
-              >
-                {employer}
-              </button>
-            ))} 
-           </div>
-
-           <div className={`job-description ${isFading ? "fade-out" : "fade-in"}`}>
-            <h1 class="job-title">{highlightCompanyName(jobDescriptions[selectedEmployer]?.title || "Company title")}</h1>
-            <p class="job-date">{jobDescriptions[selectedEmployer]?.date || "Date"}</p>
-            {Array.isArray(jobDescriptions[selectedEmployer]?.details) ? (
-              <ul>
-              {jobDescriptions[selectedEmployer].details.map((detail, index) => (
-               <li key={index}>{detail}</li>
-              ))}
-              </ul>
-              ) : (
-            <p>Select a company to view details.</p>
-            )}
-           </div>
-          </div>
-          </section>
+          <div>
+           <JobExperience />
           </div>
 
           <div id="projects" className="projectContainer">
           <section style={styles.section}>
-            <h2 style={styles.sectionHeading}>Recent Projects</h2>
+           <h2 className="sectionHeading">Recent Projects</h2>
             
             <div className="projectWrapper">
              <div className="imageContainer">
