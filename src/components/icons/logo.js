5cb6ae3e66cde91d08d16ed8844dff161689eeb1
Patrick <patrickpachacz0@gmail.com>
2025-03-07 14:07:24 -0500

    logo nav change

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,34 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const Logo = () => {
-    const polygonRef = useRef(null);
-
-    useEffect(() => {
-        if (polygonRef.current) {
-            polygonRef.current.style.animation = "drawPolygon 5s linear infinite";
-        }
-    }, []);
-
     return (
         <div style={styles.logoContainer}>
-            <svg width="80" height="80" viewBox="0 0 200 220" xmlns="http://www.w3.org/2000/svg">
-                <polygon
-                    ref={polygonRef}
-                    points="100,10 40,198 190,78 10,78 160,198"
-                    style={styles.polygon}
-                />
-                <text x="85" y="130" style={styles.text}>P</text>
+            <svg width="80" height="80" viewBox="0 40 200 220" xmlns="http://www.w3.org/2000/svg">
+                <text x="85" y="120" style={styles.text}>P</text>
             </svg>
-
-            <style>
-                {`
-                    @keyframes drawPolygon {
-                        0% { stroke-dasharray: 0, 500; stroke-dashoffset: 0; }
-                        50% { stroke-dasharray: 500, 400; stroke-dashoffset: -10; } 
-                        100% { stroke-dasharray: 500, 500; stroke-dashoffset: -500; }
-                    }
-                `}
-            </style>
         </div>
     );
 };
@@ -39,18 +16,11 @@ const styles = {
         alignItems: "center",
         justifyContent: "center",
     },
-    polygon: {
-        fill: "none",
-        stroke: "yellow",
-        strokeWidth: 5,
-        strokeDasharray: "500, 500", 
-        strokeDashoffset: "0",
-    },
     text: {
         fill: "White",
-        fontSize: "50px",
+        fontSize: "80px",
         fontWeight: "bold",
-        fontFamily: "Arial, sans-serif",
+        fontFamily: "'Dancing Script', 'Pacifico', 'Brush Script MT', cursive",
     },
 };
 
